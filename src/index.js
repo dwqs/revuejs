56cff580060b1e317ac37443b0d9637369b0f165
dwqs <461147874@qq.com>
2017-08-14 00:49:12 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import install from './install';
 
 export { default as mergeActions } from './merge-actions';
 export { default as mergeProps } from './merge-props';
-export { Modules } from './store';
+export { Modules } from './modules';
 
 const revuejs = {
     install
