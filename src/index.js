61a02b5ba7d202c9d06b60feaa2bbc53be704dac
dwqs <461147874@qq.com>
2017-08-14 00:48:21 +0800

    rename store to modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import install from './install';
 
 export { default as mergeActions } from './merge-actions';
 export { default as mergeProps } from './merge-props';
-export { Store } from './store';
+export { Modules } from './store';
 
 const revuejs = {
     install
