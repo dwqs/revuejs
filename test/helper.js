7e4c1daa25fb215582b5ff5724eedae5fb8647dd
dwqs <461147874@qq.com>
2017-08-23 23:56:53 +0800

    add test

---
+++ test/helper.js
@@ -0,0 +1,108 @@
+import test from 'ava';
+import Vue from 'vue';
+import Revuejs, { Modules, mergeActions, mergeProps } from '../dist/index';
+
+let counter;
+let modules;
+
+test.beforeEach(t => {
+    Vue.use(Revuejs);
+    counter = {
+        namespace: 'counter',
+        state: {
+            count: 0,
+            title: 'test count'
+        },
+        actions: {
+            increase (state, payload) {
+                let newCount = state.count + 1;
+                return Object.assign({}, state, {
+                    count: newCount
+                });
+            },
+            decrease (state, payload) {
+                let newCount = state.count - 1;
+                return {
+                    count: newCount
+                };
+            }
+        }
+    };
+    modules = new Modules({
+        counter
+    });
+});
+
+test('mergeProps(Array)', (t) => {
+    t.plan(2);
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count', 'counter.title'])
+    });
+    t.is(vm.count, counter.state.count);
+    t.is(vm.title, counter.state.title);
+});
+
+test('mergeProps(Object)', (t) => {
+    t.plan(2);
+    const vm = new Vue({
+        modules,
+        computed: mergeProps({
+            ct: 'counter.count',
+            tl: 'counter.title'
+        })
+    });
+    t.is(vm.ct, counter.state.count);
+    t.is(vm.tl, counter.state.title);
+});
+
+test('mergeActions(Array)', (t) => {
+    t.plan(7);
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count']),
+        methods: mergeActions(['counter.increase', 'counter.decrease'])
+    });
+
+    t.is(vm.count, 0);
+    t.is(typeof vm.increase, 'function');
+    t.is(typeof vm.decrease, 'function');
+
+    vm.increase();
+
+    t.is(vm.count, 1);
+    t.is(vm.count, counter.state.count);
+
+    vm.decrease();
+    vm.decrease();
+
+    t.is(vm.count, -1);
+    t.is(vm.count, counter.state.count);
+});
+
+test('mergeActions(Object)', (t) => {
+    t.plan(7);
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count']),
+        methods: mergeActions({
+            inc: 'counter.increase',
+            dec: 'counter.decrease'
+        })
+    });
+
+    t.is(vm.count, 0);
+    t.is(typeof vm.inc, 'function');
+    t.is(typeof vm.dec, 'function');
+
+    vm.inc();
+
+    t.is(vm.count, 1);
+    t.is(vm.count, counter.state.count);
+
+    vm.dec();
+    vm.dec();
+
+    t.is(vm.count, -1);
+    t.is(vm.count, counter.state.count);
+});
---
+++ test/modules.js
@@ -0,0 +1,212 @@
+import test from 'ava';
+import Vue from 'vue';
+import awaitTo from 'async-await-error-handling';
+import Revuejs, { Modules, mergeActions, mergeProps } from '../dist/index';
+
+let counter;
+let todo;
+let modules;
+
+test.beforeEach(t => {
+    Vue.use(Revuejs);
+    counter = {
+        namespace: 'counter',
+        state: {
+            count: 0,
+            title: 'test count'
+        },
+        actions: {
+            increase (state, payload) {
+                let newCount = state.count + payload;
+                return Object.assign({}, state, {
+                    count: newCount
+                });
+            },
+            decrease (state, payload) {
+                let newCount = state.count - payload;
+                return {
+                    count: newCount
+                };
+            },
+            resolvedPromise (state, payload) {
+                return new Promise(resolve => {
+                    let newCount = state.count + payload;
+                    resolve({
+                        count: newCount
+                    });
+                });
+            },
+            rejectedPromise (state, payload) {
+                return new Promise((resolve, reject) => {
+                    reject(new Error('rejected error'));
+                });
+            }
+        }
+    };
+    todo = {
+        namespace: 'todo',
+        state: {
+            total: 0,
+            list: []
+        },
+        actions: {
+            addTodo (state, payload) {
+                state.list.push(payload.item);
+                return {
+                    list: state.list,
+                    total: state.list.length
+                };
+            },
+            deleteTodo (state, payload) {
+                state.list.splice(payload.index, 1);
+                return Object.assign({}, state, {
+                    list: state.list,
+                    total: state.list.length
+                });
+            },
+
+            async testAsync (state, payload) {
+                let text = await Promise.resolve('testAsync');
+                return {
+                    item: text
+                };
+            },
+
+            async asyncAddTodo (state, payload) {
+                let { item } = await this.actions.testAsync(state, payload);
+                state.list.push(item);
+                return {
+                    list: state.list,
+                    total: state.list.length
+                };
+            }
+        }
+    };
+    modules = new Modules({
+        counter,
+        todo
+    });
+});
+
+test('sync actions', (t) => {
+    t.plan(5);
+    
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count']),
+        methods: mergeActions(['counter.increase', 'counter.decrease'])
+    });
+
+    t.is(vm.count, 0);
+
+    vm.increase(5);
+    
+    t.is(vm.count, 5);
+    t.is(vm.count, counter.state.count);
+
+    vm.decrease(2);
+    vm.decrease(6);
+
+    t.is(vm.count, -3);
+    t.is(vm.count, counter.state.count);
+});
+
+test('action with object style', (t) => {
+    t.plan(11);
+    
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['todo.total', 'todo.list']),
+        methods: mergeActions(['todo.addTodo', 'todo.deleteTodo'])
+    });
+
+    t.is(vm.total, 0);
+
+    vm.addTodo({
+        item: 'item1'
+    });
+    vm.addTodo({
+        item: 'item2'
+    });
+    
+    t.is(vm.total, 2);
+    t.is(vm.total, todo.state.total);
+    t.deepEqual(vm.list, ['item1', 'item2']);
+    t.deepEqual(vm.list, todo.state.list);
+    t.deepEqual(vm.list[1], 'item2');
+
+    vm.deleteTodo({
+        index: 0
+    });
+
+    t.is(vm.total, 1);
+    t.is(vm.total, todo.state.total);
+    t.deepEqual(vm.list, ['item2']);
+    t.deepEqual(vm.list, todo.state.list);
+    t.deepEqual(vm.list[0], 'item2');
+});
+
+test.serial('action return resolved promise', async t => {
+    t.plan(5);
+    
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count']),
+        methods: mergeActions(['counter.resolvedPromise'])
+    });
+
+    t.is(vm.count, 0);
+
+    let res = vm.resolvedPromise(100);
+    const [err, data] = await awaitTo(res);
+    
+    t.is(vm.count, 100);
+    t.falsy(err, false);
+    t.deepEqual({count: 100}, data);
+    t.is(typeof res.then, 'function');
+});
+
+test.serial('action return rejected promise', async t => {
+    t.plan(7);
+    
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['counter.count']),
+        methods: mergeActions(['counter.rejectedPromise', 'counter.increase'])
+    });
+
+    t.is(vm.count, 0);
+    
+    vm.increase(50);
+
+    t.is(vm.count, 50);
+    
+    let res = await t.throws(vm.rejectedPromise(20));
+    const [err, data] = await awaitTo(vm.rejectedPromise(20));
+    
+    t.is(res.message, 'rejected error');
+    t.is(err.message, 'rejected error');
+    t.is(vm.count, 50);
+    t.is(data, null);
+});
+
+test.serial('composing actions with async/await', async t => {
+    t.plan(5);
+
+    const vm = new Vue({
+        modules,
+        computed: mergeProps(['todo.total', 'todo.list']),
+        methods: mergeActions(['todo.asyncAddTodo'])
+    });
+
+    t.is(vm.total, 0);
+    t.deepEqual(vm.list, []);
+
+    await vm.asyncAddTodo();
+
+    console.log('rrrrrr', vm.total, vm.list);
+
+    t.is(vm.total, 1);
+    t.deepEqual(vm.list, todo.state.list);
+    t.deepEqual(vm.list, ['testAsync']);
+});
