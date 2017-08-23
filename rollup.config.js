8894907ed5d4c3985da278436966ab8c3e379e7c
dwqs <461147874@qq.com>
2017-08-24 00:48:09 +0800

    update rollup build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
 import babel from 'rollup-plugin-babel';
 import cleanup from 'rollup-plugin-cleanup';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: 'src/index.js',
     format: 'umd',
-    moduleName: 'revuejs',
+    moduleName: 'Revuejs',
     dest: 'dist/index.js',
     plugins: [
+        resolve({
+            customResolveOptions: 'node_modules',
+            jsnext: true,
+            main: true
+        }),
+        commonjs({
+            namedExports: {
+
+            }
+        }),
         babel({
             exclude: 'node_modules/**',
             externalHelpers: false,
             babelrc: false,
+            runtimeHelpers: true,
             presets: [
                 [
                     'es2015',
@@ -18,7 +31,8 @@ export default {
                         'modules': false
                     }
                 ]
-            ]
+            ],
+            plugins: ['transform-runtime']
         }),
         cleanup()
     ]
