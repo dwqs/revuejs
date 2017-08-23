afe1b58de325404279397047daffda6110a9f5d8
dwqs <461147874@qq.com>
2017-08-23 23:56:33 +0800

    update repos modules

--- examples/modules/repos.js
+++ examples/modules/repos.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import awaitTo from 'async-await-error-handling';
 
 export default {
     namespace: 'repos',
@@ -9,18 +10,12 @@ export default {
     actions: {
         async getYourRepos (state, payload) {
             let {username} = payload;
-            let data = {};
-            let error = '';
-
-            try {
-                data = await axios.get(`https://api.github.com/users/${username}/repos`);
-            } catch (e) {
-                error = e.message;
-            }
+    
+            const [err, data] = await awaitTo(axios.get(`https://api.github.com/users/${username}/repos`));
 
             return {
                 all: data.data ? data.data.length : 0,
-                error
+                error: !err ? '' : err
             };
         }
     }  
