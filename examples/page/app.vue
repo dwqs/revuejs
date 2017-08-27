224d6bf555191aa5f10916a9f1ac4379b500d184
dwqs <461147874@qq.com>
2017-08-27 19:34:55 +0800

    same prop in module

--- examples/page/app.vue
+++ examples/page/app.vue
@@ -3,17 +3,31 @@
         <h3>Examples for Revuejs: </h3>
         <ul class="examples-list">
             <li>
-                <router-link to='counter'>Counter</router-link>
+                <router-link to='counter'>{{countTitle}}</router-link>
             </li>
             <li>
-                <router-link to='todo'>ToDo</router-link>
+                <router-link to='todo'>{{todoTitle}}</router-link>
             </li>
             <li>
-                <router-link to='repos'>Repos</router-link>
+                <router-link to='repos'>{{reposTitle}}</router-link>
             </li>
         </ul>
     </div>
 </template>
+
+<script>
+    import {mergeActions, mergeProps} from 'revuejs';
+
+    export default {
+        computed: {
+            ...mergeProps({
+                'countTitle': 'counter.title',
+                'todoTitle': 'todo.title',
+                'reposTitle': 'repos.title'
+            })
+        },
+    }
+</script>
 
 <style lang="less"> 
     .examples-list {
